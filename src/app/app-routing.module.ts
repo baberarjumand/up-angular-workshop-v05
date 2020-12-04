789f95d46e60ac7e20ada7369d0ec34e94241d96
baberarjumand <baber.arjumand@gmail.com>
2020-12-04 16:05:37 +0500

    setup initial set of pages to start workshop, imported bootstrap via CDN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { UserFormComponent } from './pages/user-form/user-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user-form',
+    component: UserFormComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'user-form',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
